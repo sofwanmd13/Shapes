6dc6a93f8ae3b44376084a70308835ff62bf1671
sofwanmd13 <112913022+sofwanmd13@users.noreply.github.com>
2022-09-21 16:05:56 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,7 +1,7 @@
 
 public class Square extends Rectangle{
 	
-	public Square(double side) {
+	public Square(double side) {  //Since Square is basically a rectangle with same sides, we can just extend the Rectangle class to use it for square too
 		super(side, side);
 	}
 }
