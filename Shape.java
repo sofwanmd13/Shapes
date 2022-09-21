a314972f096eaa66f3a05f0a7b94c28b26ff1586
sofwanmd13 <112913022+sofwanmd13@users.noreply.github.com>
2022-09-21 16:57:10 -0400

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,6 +1,6 @@
-
 public abstract class Shape {
 	
-	public abstract double calculateArea();  //Abstract methods for parent class
+	public abstract double calculateArea();  //Abstract methods for superclass
 	public abstract double calculatePerimeter(); 
+	public abstract String gettype();
 }
