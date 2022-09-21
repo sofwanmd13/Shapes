231801c37e67752fe943627308915c82ef475f7e
sofwanmd13 <112913022+sofwanmd13@users.noreply.github.com>
2022-09-21 16:28:56 -0400

    Update TestShapes.java

--- TestShapes.java
+++ TestShapes.java
@@ -7,41 +7,44 @@ public class TestShapes {
 		String shape;
 		
 		do {
-			System.out.println("Select the type of shape to add, or Q to quit: ");  //Asking user for input on which type of shape is being entered, or if they want to quit to end
+			System.out.println("Select the type of shape to add, or Q to quit: ");  //Asking user for input on which type of shape is being entered, or if they want to quit
 			shape = input.next();
 			
 			if (shape.equals("rectangle")||shape.equals("Rectangle")) {  //When Rectangle shape type is entered
-				System.out.println("Enter Length: ");
+				System.out.println("Enter Length: ");   //Prompting to enter variables for shape
 				double length = input.nextDouble();
 				
 				System.out.println("Enter Width: ");
 				double width = input.nextDouble();
 				
-				shapes.add(new Rectangle(length, width));
+				shapes.add(new Rectangle(length, width));   //Creating new shape of type Rectangle and adding it to ArrayList
 			}
 			
 			else if (shape.equals("square")||shape.equals("Square")) {  //When Square shape type is entered
-				System.out.println("Enter Length of side: ");
+				System.out.println("Enter Length of side: ");   //Prompting to enter variables for shape
 				double length = input.nextDouble();
 				
-				shapes.add(new Square(length));
+				shapes.add(new Square(length));   //Creating new shape of type Square and adding it to ArrayList
 			}
 			
 			else if (shape.equals("circle")||shape.equals("Circle")) {  //When Circle shape type is entered
-				System.out.println("Enter Radius: ");
+				System.out.println("Enter Radius: ");   //Prompting to enter variables for shape
 				double radius = input.nextDouble();
 				
-				shapes.add(new Circle(radius));
+				shapes.add(new Circle(radius));   //Creating new shape of type Circle and adding it to ArrayList
 			}
 			
 			else if (shape.equals("triangle")||shape.equals("Triangle")) {  //When Triangle shape type is entered
-				System.out.println("Enter height: ");
-				double height = input.nextDouble();
+				System.out.println("Enter side1: ");   //Prompting to enter variables for shape
+				double side1 = input.nextDouble();
 
-				System.out.println("Enter base: ");
-				double width = input.nextDouble();
+				System.out.println("Enter side2: ");
+				double side2 = input.nextDouble();
 				
-				shapes.add(new Triangle(height, width));
+				System.out.println("Enter side3: ");
+				double side3 = input.nextDouble();
+				
+				shapes.add(new Triangle(side1, side2, side3));   //Creating new shape of type Triangle and adding it to ArrayList
 			}
 			
 			else if (!(shape.equals("Q")||shape.equals("q"))) {  //When no proper shape type, or Q/q is entered
